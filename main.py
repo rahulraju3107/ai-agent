9ab952705af7b09503b5e38da4fa3d9e4540a92d
rahulraju3107 <rahulraju3107@gmail.com>
2026-02-05 22:47:30 +1100

    Add parser object for user input as terminal argument

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 import os
+import argparse
+
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-if api_key == None:
-    raise RuntimeError("API key not found")
-
-client = genai.Client(api_key=api_key)
-
-response = client.models.generate_content(
-        model="gemini-2.5-flash", 
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
-        )
-if not response.usage_metadata:
-    raise RuntimeError("Gemini API response appears to be malformed")
-
 
 def main():
+    # Parser object
+    parser = argparse.ArgumentParser(description="ai-agent")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key == None:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set")
+
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", 
+        contents=args.user_prompt,
+    )
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
+
     print("Prompt tokens:", response.usage_metadata.prompt_token_count)
     print("Response tokens:", response.usage_metadata.candidates_token_count)
     print("Response:")
